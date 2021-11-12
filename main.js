534c5ee2e7cda696c20ba10cfc803d5c6c477cfa
Sajal-Rathore <94166065+Sajal-Rathore@users.noreply.github.com>
2021-11-12 13:57:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function cuber ()  {
+    var result1 = document.getElementById("input1").value;
+    if(result1=="3x3 cube") {
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from any toy shop and it is not that much difficult to solve it and it is having 26 pieces in total and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="c.jpg" onclick="c1()">'
+    }
+    else if(result1=="4x4 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from any toy shop and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="4c1.jpg" onclick="c2()">'
+    }
+    else if(result1=="5x5 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from any toy shop and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="5x5.jpg" onclick="c3()">'
+    }
+    else if(result1=="6x6 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from amazon or flipkart and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="6x6.jpg" onclick="c4()">'
+    }
+    else if(result1=="7x7 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from amazon or flipkart and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="7x7.jpg" onclick="c5()">'
+    }
+    else if(result1=="8x8 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from amazon or flipkart and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="8x8.jpg" onclick="c6()">'
+    }
+    else if(result1=="9x9 cube"){
+        document.getElementById("result").innerHTML = 'This is a variety of cube which can be purchased from amazon or flipkart and it is not that much difficult to solve it and here is a link to buy it';
+        document.getElementById("result1").innerHTML = '<img id="cube2" src="9x9.jpg" onclick="c7()">'
+    }
+    else {
+        document.getElementById("result").innerHTML = "Search for a cube given above";
+    }; 
+}
+function c1(){
+    window.location="https://www.amazon.in/Cubelelo-Warrior-Stickerless-Speedcube-Puzzle/dp/B0816P5ZJX/ref=sr_1_2_sspa?crid=188GS73W6LCTW&dchild=1&keywords=3x3+rubiks+cube+speed+cube&qid=1632819894&sprefix=3x3+%2Caps%2C299&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNUY0MDdHSkhJQTZPJmVuY3J5cHRlZElkPUEwMDg4NzIzVzYwSkFQMk1TSjdFJmVuY3J5cHRlZEFkSWQ9QTA1NjI2MzVXMlhFRE9GNjdIMEsmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl";
+}
+
+function c2(){
+    window.location="https://www.amazon.in/Cubelelo-MeiLong-Stickerless-Puzzle-Speed/dp/B07TD2Q4SP/ref=sr_1_4_sspa?dchild=1&keywords=4x4+rubiks+cube+speed+cube&qid=1632820881&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFUUjJPTldDM0FGOUMmZW5jcnlwdGVkSWQ9QTAxOTU5MDkxNUhCQjZVTjNST1NUJmVuY3J5cHRlZEFkSWQ9QTA5ODkxNzIyU1hFOTJVNEQzT1lTJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==";
+}
+
+function c3(){
+    window.location="https://www.amazon.in/Cubelelo-QiYi-QiZheng-5x5-Stickerless/dp/B07FJW8LSG/ref=sr_1_1?crid=3G1L5PI4Y5B7M&dchild=1&keywords=5x5+rubiks+cube&qid=1633506737&qsid=257-4832732-3165511&sprefix=5x5+%2Caps%2C309&sr=8-1&sres=B07FJW8LSG%2CB07NCPHYLM%2CB07QSWX8CG%2CB07NBJ5WRN%2CB07HJDW2R9%2CB07YWN7D2F%2CB07TLLVLJY%2CB088MLPM1Q%2CB07WPSJ6LR%2CB07SPGRGGN%2CB08T98F7T6%2CB08M5N4QYP%2CB08M63C43N%2CB08WQ1338F%2CB07NDKZQ5V%2CB07ZC9DQW8%2CB08M5C5XSY%2CB07P2T5DSY%2CB09HJ9X9H3%2CB08CSL4DNL&srpt=PUZZLES";
+}
+
+function c4(){
+    window.location="https://www.amazon.in/Cubelelo-YuShi-Stickerless-Magnetic-Puzzle/dp/B081H49ZVQ/ref=sr_1_1?dchild=1&keywords=6x6+rubiks+cube&qid=1633506951&qsid=257-4832732-3165511&sr=8-1&sres=B081H49ZVQ%2CB07S1Z77RS%2CB07S6SZ39L%2CB07WYL7GCC%2CB08NGYX162%2CB07NCPHYLM%2CB07P2T5DSY%2CB07QSWX8CG%2CB083S2T1HM%2CB07H5L1L1R%2CB085BQGMF9%2CB07TLLVLJY%2CB088MLPM1Q%2CB082RDCFG2%2CB07WPJ7P8Q%2CB07STKP1XR%2CB07D6QKPT4%2CB07FRGSV71%2CB07ZYM5SK5%2CB07S5YDLJY&srpt=PUZZLES";
+}
+
+function c5(){
+    window.location="https://www.amazon.in/Cubelelo-Stickerless-Magnetic-Puzzle-Speed/dp/B07WPJ7P8Q/ref=sr_1_2?dchild=1&keywords=7x7+rubiks+cube&qid=1633507008&qsid=257-4832732-3165511&sr=8-2&sres=B07FRGSV71%2CB07WPJ7P8Q%2CB082RQBB41%2CB08SMFL986%2CB08BKG3B7C%2CB07STKP1XR%2CB08B5BSBB7%2CB07WQ24P75%2CB08NPMG9RS%2CB07MR7757J%2CB08VDRKVXG%2CB08PD5GRDG%2CB07H8DPWCZ%2CB07NZKNJFN%2CB07S1Z77RS%2CB07H5L1L1R%2CB07QSWX8CG%2CB07S6SZ39L%2CB07WYL7GCC%2CB07GN1FW3D&srpt=PUZZLES";
+}
+
+function c6(){
+    window.location="https://www.amazon.in/Cubelelo-MoFang-JiaoShi-Puzzle-Stickerless/dp/B07PQ7TSB8/ref=sr_1_1?dchild=1&keywords=8x8+rubiks+cube&qid=1633507033&qsid=257-4832732-3165511&sr=8-1&sres=B07PQ7TSB8%2CB07Z22HZHQ%2CB08RZ33QMW%2CB07W3N2W5S%2CB0849TJK6V%2CB07S1Z77RS%2CB07T1RF6GM%2CB0863Q9NKX%2CB08LYZZ9SG%2CB07YDT6MWZ%2CB00EPDY59A%2CB07ZD8D927%2CB08DFQSCHX%2CB08NGYX162%2CB085BQGMF9%2CB07Y9D1S98%2CB07VXPY12X%2CB083S2T1HM%2CB07YSWNCGT%2CB07V7TRMR9&srpt=PUZZLES";
+}
+
+function c7(){
+    window.location="https://www.amazon.in/ETERNAL-Sengso-9x9x9-Speed-Puzzle/dp/B07ZD8D927/ref=sr_1_4?dchild=1&keywords=9x9+rubiks+cube&qid=1633507068&qsid=257-4832732-3165511&sr=8-4&sres=B07VXPY12X%2CB07Z23GV9Y%2CB07YDT6MWZ%2CB07ZD8D927%2CB07WPJ7P8Q%2CB07PQ7TSB8%2CB07W83CVN7%2CB07FJW8LSG%2CB08DFQSCHX%2CB07Q7G5NB5%2CB07S1Z77RS%2CB07VN2BPZJ%2CB0192XQ3JC%2CB07WYXFH61%2CB07V7TRMR9%2CB07WYL7GCC%2CB08GWQR9V1%2CB07STKP1XR%2CB07NCPHYLM%2CB08RZ33QMW&srpt=PUZZLES";
+}
